d320bed3f3b9d2a113e77129d78722b9ec6986ca
AbeerOsman <abeerjilani90@gmail.com>
2025-12-28 14:53:29 +0300

    API Hidden

---
+++ MovieApp_Team(3)_M/ViewModel/HighRatedViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  HighRatedViewModel.swift
+//  MovieApp_Team(3)_M
+//
+//  Created by Abeer Jeilani Osman  on 06/07/1447 AH.
+//
+
+
+import Foundation
+import Combine
+
+@MainActor
+class HighRatedViewModel: ObservableObject {
+
+    @Published var movies: [MoviesInfo] = []
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    
+
+    /// Get movies filtered by genre / category
+    func moviesByGenre(_ genre: String) -> [MoviesInfo] {
+        movies.filter { $0.genre.contains(genre) }
+    }
+
+
+    // Load movies
+    func loadMovies() async {
+        isLoading = true
+        defer { isLoading = false }
+
+        do {
+            movies = try await getMovies()
+        } catch {
+            errorMessage = error.localizedDescription
+            print("Error loading movies:", error)
+        }
+    }
+
+    // Networking
+    private func getMovies() async throws -> [MoviesInfo] {
+        let endpoint = "https://api.airtable.com/v0/appsfcB6YESLj4NCN/movies"
+        // Convert string url into URL object
+        guard let url = URL(string: endpoint) else {
+            throw MoviesError.invalidURL
+        }
+
+        // Create a URLRequest to add the Authorization header
+        var request = URLRequest(url: url)
+        print("token is: \(APIKey.airtable)")
+        request.setValue(
+            "Bearer \(APIKey.airtable)",
+            forHTTPHeaderField: "Authorization"
+        )
+        // Call network
+        let (data, response) = try await URLSession.shared.data(for: request)
+        // Check HTTP response
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw MoviesError.invalidResponse
+        }
+        // Decode JSON
+        let decoder = JSONDecoder()
+        let result = try decoder.decode(MoviesResponse.self, from: data)
+        // Return array of MoviesInfo
+        return result.records.map { $0.fields }
+    }
+}
